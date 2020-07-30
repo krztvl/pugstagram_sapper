ba463be42c5987564489cc29d8ced9e1804a286f
Krztvl <c.penatroncoso@gmail.com>
2020-07-29 23:27:36 -0400

    Layout, y por fin configurado

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
-import sirv from "sirv";
-import compression from "compression";
-import polka from "polka";
-import * as sapper from "@sapper/server";
+import sirv from 'sirv';
+import compression from 'compression';
+import polka from 'polka';
+import * as sapper from '@sapper/server';
 
-const { Node_ENV } = process.env;
-const dev = Node_ENV === "development";
+const { PORT, NODE_ENV } = process.env;
+const dev = NODE_ENV === 'development';
 
 polka()
   .use(
     compression({ threshold: 0 }),
-    sirv("static", { dev }),
-    sapper.middleweare()
+    sirv('static', { dev }),
+    sapper.middleware()
   )
-  .listen(PORT, (err) => {
-    if (err) console.log("error", err);
+  .listen(PORT, err => {
+    if (err) console.log('error', err);
   });
